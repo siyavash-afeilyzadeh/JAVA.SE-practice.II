2f43848845bccc974959f23cac5430d3a35c01ba
siyavash-afeilyzadeh <siyavash.afeilyzadeh@gmail.com>
2026-06-29 20:47:22 +0330

    Initial Commit

---
+++ src/main/java/black/model/Person.java
@@ -0,0 +1,17 @@
+package black.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@SuperBuilder
+
+public class Person {
+    private int id;
+    private String name;
+    private String family;
+    private int age;
+
+}
