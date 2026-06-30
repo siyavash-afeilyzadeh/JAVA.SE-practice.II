973b69d9cec48df6dbcd4353bbcfeb34c8822602
siyavash-afeilyzadeh <siyavash.afeilyzadeh@gmail.com>
2026-06-30 16:52:20 +0330

    Initial Commit

--- src/main/java/black/da/OrderDA.java
+++ src/main/java/black/da/OrderDA.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class OrderDA {
-    public void save(Order order) throws SQLException{
+    public void save(Order order) throws SQLException {
         Connection connection = DriverManager.getConnection(
                 "jdbc:oracle:thin:@localhost:1521:xe",
                 "javase", "java123"
@@ -21,17 +21,49 @@ public class OrderDA {
         preparedStatement.setString(2, order.getName());
         preparedStatement.setFloat(3, order.getPrice());
         preparedStatement.setInt(4, order.getQuantity());
-
         preparedStatement.execute();
         preparedStatement.close();
+
         connection.close();
+        System.out.println("Order saved successfully");
     }
 
-    public void update(Order order) throws SQLException{
+    public void update(Order order) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe",
+                "javase", "java123"
+        );
 
+        PreparedStatement preparedStatment = connection.prepareStatement(
+                "update orders set name = ?, price = ?, quantity = ? where id=?"
+        );
+        preparedStatment.setString(1, order.getName());
+        preparedStatment.setFloat(2, order.getPrice());
+        preparedStatment.setInt(3, order.getQuantity());
+        preparedStatment.setInt(4, order.getId());
+
+        preparedStatment.execute();
+        preparedStatment.close();
+
+        connection.close();
+        System.out.println("Order updated successfully");
     }
 
-    public void delete(int id) throws SQLException{
+    public void delete(int id) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe",
+                "javase","java123"
+        );
+
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "delete from orders where id=?"
+        );
+        preparedStatement.setInt(1,id);
+        preparedStatement.execute();
+        preparedStatement.close();
+
+        connection.close();
+        System.out.println("Order has been deleted");
 
     }
 }
--- src/test/java/black/da/OrderDATest.java
+++ src/test/java/black/da/OrderDATest.java
@@ -6,16 +6,18 @@ import java.sql.SQLException;
 
 public class OrderDATest {
     public static void main(String[] args) throws SQLException {
-        Order order = Order
-                .builder()
-                .id(1)
-                .name("Mobile")
-                .price(2300.55F)
-                .quantity(2)
-                .build();
-        System.out.println(order);
+//        Order order = Order
+//                .builder()
+//                .id(1)
+//                .name("Laptop")
+//                .price(1000.00F)
+//                .quantity(1)
+//                .build();
+//        System.out.println(order);
 
         OrderDA orderDA = new OrderDA();
-        orderDA.save(order);
+//        orderDA.save(order);
+//        orderDA.update(order);
+        orderDA.delete(1);
     }
 }
